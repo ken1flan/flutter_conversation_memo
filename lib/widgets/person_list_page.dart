caebe248f7f03d6daa193655fedfc3fb5d2272ea
ken1flan <ken1flan@gmail.com>
2021-06-27 08:12:28 +0900

    Personモデルの組み込み

--- lib/widgets/person_list_page.dart
+++ lib/widgets/person_list_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
+import 'package:hive/hive.dart';
+import 'package:hive_flutter/hive_flutter.dart';
+import 'package:timeago/timeago.dart' as timeago;
 
 import 'package:flutter_conversation_memo/widgets/drawer.dart';
 import 'package:flutter_conversation_memo/widgets/person_page.dart';
+import 'package:flutter_conversation_memo/models/person.dart';
 
 class PersonListPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    var theme = Theme.of(context);
     var localizations = AppLocalizations.of(context);
 
     return Scaffold(
@@ -14,8 +19,64 @@ class PersonListPage extends StatelessWidget {
         title: Text(localizations.appTitle),
       ),
       drawer: createDrawer(context),
-      body: Center(
-        child: Text(localizations.notFound),
+      body: ValueListenableBuilder(
+        valueListenable: Person.box().listenable(),
+        builder: (context, Box<Person> box, _) {
+          if (box.values.isEmpty) {
+            return Center(
+              child: Text(localizations.notFound),
+            );
+          }
+          return ListView.builder(
+              itemCount: box.values.length,
+              itemBuilder: (context, index) {
+                var currentPerson = box.getAt(index);
+                return Card(
+                  clipBehavior: Clip.antiAlias,
+                  child: InkWell(
+                    onTap: () {
+                      _editPerson(context, index);
+                    },
+                    onLongPress: () {
+                      // tood
+                    },
+                    child: Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Column(
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: <Widget>[
+                          SizedBox(height: 5),
+                          Text(
+                            currentPerson.name,
+                            overflow: TextOverflow.ellipsis,
+                          ),
+                          Row(
+                            children: [
+                              Text(
+                                  timeago.format(currentPerson.updated_at,
+                                      locale: 'ja'),
+                                  style: TextStyle(
+                                    color: theme.disabledColor,
+                                    fontSize: 12,
+                                  )),
+                              SizedBox(width: 5),
+                              Text(
+                                currentPerson.tags_string,
+                                style: TextStyle(
+                                  color: theme.disabledColor,
+                                  fontSize: 12,
+                                ),
+                              )
+                            ],
+                          ),
+                          SizedBox(height: 5),
+                        ],
+                      ),
+                    ),
+                  ),
+                );
+              });
+        },
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
@@ -32,6 +93,14 @@ class PersonListPage extends StatelessWidget {
         context,
         MaterialPageRoute(
           builder: (context) => PersonPage(),
+        ));
+  }
+
+  void _editPerson(BuildContext context, int index) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => PersonPage(index: index),
         ));
   }
 }
--- lib/widgets/person_page.dart
+++ lib/widgets/person_page.dart
@@ -1,39 +1,119 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_conversation_memo/models/person.dart';
 
 class PersonPage extends StatefulWidget {
+  final formKey = GlobalKey<FormState>();
+  final int index;
+
+  PersonPage({this.index});
+
   @override
-  _PersonPageState createState() => _PersonPageState();
+  _PersonPageState createState() => _PersonPageState(index);
 }
 
 class _PersonPageState extends State<PersonPage> {
+  int index;
+  String name = '';
+  String memo = '';
+  String tags_string = '';
+  DateTime created_at;
+  DateTime updated_at;
+
+  _PersonPageState(index) {
+    this.index = index;
+    if (index != null) {
+      var person = Person.getAt(index);
+      name = person?.name;
+      memo = person?.memo;
+      tags_string = person?.tags_string;
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
+    final titleString = '人の新規作成 | 会話ネタ帳';
+    final nameEditingController =
+        TextEditingController.fromValue(TextEditingValue(
+      text: name,
+      selection: TextSelection.collapsed(offset: name.length),
+    ));
+    final memoEditingController =
+        TextEditingController.fromValue(TextEditingValue(
+      text: memo,
+      selection: TextSelection.collapsed(offset: memo.length),
+    ));
+    final tagsStringEditingController =
+        TextEditingController.fromValue(TextEditingValue(
+      text: tags_string,
+      selection: TextSelection.collapsed(offset: tags_string.length),
+    ));
+
     return Scaffold(
         appBar: AppBar(
-          title: Text('人'),
+          title: Text(titleString),
         ),
         body: Padding(
             padding: const EdgeInsets.all(16),
             child: ListView(
               children: [
                 TextField(
+                  controller: nameEditingController,
                   decoration: const InputDecoration(
                     labelText: '名前',
                   ),
+                  onChanged: (value) {
+                    setState(() {
+                      name = value;
+                    });
+                  },
                 ),
                 TextField(
+                  controller: memoEditingController,
                   decoration: const InputDecoration(
                     labelText: 'メモ',
                   ),
                   maxLines: 10,
+                  onChanged: (value) {
+                    setState(() {
+                      memo = value;
+                    });
+                  },
                 ),
                 TextField(
+                  controller: tagsStringEditingController,
                   decoration: const InputDecoration(
                     labelText: 'タグ',
                     hintText: 'スペースで区切って入力してください。',
                   ),
+                  onChanged: (value) {
+                    setState(() {
+                      tags_string = value;
+                    });
+                  },
+                ),
+                Padding(
+                  padding: const EdgeInsets.only(top: 32),
+                  child: ElevatedButton(
+                    key: Key('saveButton'),
+                    onPressed: onFormSubmit,
+                    child: Text('保存'),
+                  ),
                 ),
               ],
             )));
+  }
+
+  void onFormSubmit() {
+    var person;
+    if (index == null) {
+      person = Person(name, memo, tags_string, created_at, updated_at);
+    } else {
+      person = Person.getAt(index);
+      person.name = name;
+      person.memo = memo;
+      person.tags_string = tags_string;
+    }
+    person.save();
+    Navigator.of(context).pop();
   }
 }
