294b5bbd152967b180bd764ea75662f739fc5fe1
ken1flan <ken1flan@gmail.com>
2021-07-11 08:22:53 +0900

    Extract topic summary
    Topicの一覧で使うカードをウィジェット化します。
    同時にそこで不便だったTopicクラスを少し変更しています。

---
+++ lib/widgets/topic_summary.dart
@@ -0,0 +1,77 @@
+import 'package:flutter/material.dart';
+import 'package:timeago/timeago.dart' as timeago;
+import 'package:flutter_conversation_memo/widgets/topic_page.dart';
+import 'package:flutter_conversation_memo/models/topic.dart';
+
+class TopicSummary extends Card {
+  //
+  TopicSummary(BuildContext context, int index, Topic topic)
+      : super(
+          clipBehavior: Clip.antiAlias,
+          child: InkWell(
+            onTap: () {
+              _editTopic(context, index);
+            },
+            onLongPress: () {
+              showDialog(
+                  context: context,
+                  barrierDismissible: false,
+                  builder: (context) {
+                    return AlertDialog(
+                      content: Text('${topic.summary}を消しますが、よろしいですか？'),
+                      actions: <Widget>[
+                        TextButton(
+                          onPressed: () => Navigator.of(context).pop(),
+                          child: Text('No'),
+                        ),
+                        TextButton(
+                          onPressed: () async {
+                            await Topic.box().deleteAt(index);
+                            Navigator.of(context).pop();
+                          },
+                          child: Text('Yes'),
+                        )
+                      ],
+                    );
+                  });
+            },
+            child: Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    SizedBox(height: 5),
+                    Text(
+                      topic.summary,
+                      overflow: TextOverflow.ellipsis,
+                    ),
+                    Row(
+                      children: [
+                        Text(timeago.format(topic.updated_at, locale: 'ja'),
+                            style: TextStyle(
+                              color: Theme.of(context).disabledColor,
+                              fontSize: 12,
+                            )),
+                        SizedBox(
+                          width: 5,
+                        ),
+                        Text(topic.tags_string,
+                            style: TextStyle(
+                                color: Theme.of(context).disabledColor,
+                                fontSize: 12)),
+                      ],
+                    ),
+                    SizedBox(height: 5),
+                  ]),
+            ),
+          ),
+        );
+
+  static void _editTopic(BuildContext context, int index) {
+    Navigator.push(
+        context,
+        MaterialPageRoute(
+          builder: (context) => TopicPage(index: index),
+        ));
+  }
+}
--- test/widgets/topic_list_page_test.dart
+++ test/widgets/topic_list_page_test.dart
@@ -1,5 +1,6 @@
 import 'package:flutter_test/flutter_test.dart';
 
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:hive/hive.dart';
 import 'package:flutter_conversation_memo/widgets/topic_list_page.dart';
@@ -7,85 +8,87 @@ import 'package:flutter_conversation_memo/models/topic.dart';
 import '../widget_test_helper.dart';
 
 void main() async {
-  await initializeTest();
-
-  setUp(() async {
-    await initializeExample();
+  setUpAll(() async {
+    final path = Directory.current.path;
+    Hive.init(path);
+    await Topic.initialize(memory_box: true);
   });
 
   tearDown(() async {
-    await finalizeExample();
+    var box = Topic.box();
+    if (box.isNotEmpty) {
+      await Topic.box().clear();
+    }
   });
 
-  testWidgets('Topicが0個のとき、ひとつもないことが表示されていること', (WidgetTester tester) async {
-    await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
+  group('Topicが0個のとき', () {
+    testWidgets('Topicが0個のとき、ひとつもないことが表示されていること', (WidgetTester tester) async {
+      await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
+      await tester.pumpAndSettle();
 
-    expect(find.text('まだありません。'), findsOneWidget);
+      expect(find.text('まだありません。'), findsOneWidget);
+    });
+
+    testWidgets('新規作成ボタンを押したとき、新規追加ページが開くこと', (WidgetTester tester) async {
+      await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
+      await tester.tap(find.byIcon(Icons.add));
+      await tester.pump();
+
+      expect(find.text('話題の新規作成 | 会話ネタ帳', skipOffstage: false), findsOneWidget);
+    });
   });
 
-  testWidgets('Topicが1個のとき、表示されていること', (WidgetTester tester) async {
-    var box = Hive.box<Topic>(topicBoxName);
-    await box.add(Topic('Summary', 'Memo', '', DateTime.now(), DateTime.now()));
+  group('Topicが1個のとき', () {
+    setUp(() async {
+      var box = Topic.box();
+      await box
+          .add(Topic('Summary', 'Memo', '', DateTime.now(), DateTime.now()));
+    });
 
-    await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
+    testWidgets('表示されていること', (WidgetTester tester) async {
+      await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
 
-    expect(find.text('Summary'), findsOneWidget);
-  });
+      expect(find.text('Summary'), findsOneWidget);
+    });
 
-  testWidgets('新規作成ボタンを押したとき、新規追加ページが開くこと', (WidgetTester tester) async {
-    await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
-    await tester.tap(find.byIcon(Icons.add));
-    await tester.pump();
+    testWidgets('タップしたとき、編集後、変更が反映されていること', (WidgetTester tester) async {
+      await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
+      await tester.tap(find.text('Summary'));
+      await tester.pumpAndSettle();
 
-    expect(find.text('話題の新規作成 | 会話ネタ帳', skipOffstage: false), findsOneWidget);
-  });
+      expect(find.text('話題の編集 | 会話ネタ帳', skipOffstage: false), findsOneWidget);
 
-  testWidgets('Topicをタップしたとき、編集後、変更が反映されていること', (WidgetTester tester) async {
-    var box = Hive.box<Topic>(topicBoxName);
-    await box.add(Topic('Summary', 'Memo', '', DateTime.now(), DateTime.now()));
-    final index = 0;
+      final index = 0;
+      await tester.enterText(
+          find.byKey(ValueKey('summaryTextField${index.toString()}')), 'サマリー');
+      await tester.tap(find.text('保存'));
+      await tester.pumpAndSettle();
 
-    await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
-    await tester.tap(find.text('Summary'));
-    await tester.pumpAndSettle();
+      expect(find.text('サマリー'), findsOneWidget);
+    });
 
-    expect(find.text('話題の編集 | 会話ネタ帳', skipOffstage: false), findsOneWidget);
+    testWidgets('Topicを長くタップしたとき、削除ダイアログが表示され、Yesを押したら消えること',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
+      await tester.longPress(find.text('Summary'));
+      await tester.pump();
 
-    await tester.enterText(
-        find.byKey(ValueKey('summaryTextField${index.toString()}')), 'サマリー');
-    await tester.tap(find.text('保存'));
-    await tester.pumpAndSettle();
+      await tester.tap(find.text('Yes'));
+      await tester.pump();
 
-    expect(find.text('サマリー'), findsOneWidget);
-  });
+      expect(find.text('まだありません。'), findsOneWidget);
+    });
 
-  testWidgets('Topicを長くタップしたとき、削除ダイアログが表示され、Yesを押したら消えること',
-      (WidgetTester tester) async {
-    var box = Hive.box<Topic>(topicBoxName);
-    await box.add(Topic('Summary', 'Memo', '', DateTime.now(), DateTime.now()));
+    testWidgets('Topicを長くタップしたとき、削除ダイアログが表示され、Noを押したら消えること',
+        (WidgetTester tester) async {
+      await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
+      await tester.longPress(find.text('Summary'));
+      await tester.pump();
 
-    await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
-    await tester.longPress(find.text('Summary'));
-    await tester.pump();
+      await tester.tap(find.text('No'));
+      await tester.pump();
 
-    await tester.tap(find.text('Yes'));
-    await tester.pump();
-
-    expect(find.text('まだありません。'), findsOneWidget);
-  });
-
-  testWidgets('Topicを長くタップしたとき、削除ダイアログが表示され、Noを押したら消えること',
-      (WidgetTester tester) async {
-    var box = Hive.box<Topic>(topicBoxName);
-    await box.add(Topic('Summary', 'Memo', '', DateTime.now(), DateTime.now()));
-
-    await tester.pumpWidget(wrapWithMaterial(TopicListPage()));
-    await tester.longPress(find.text('Summary'));
-    await tester.pump();
-
-    await tester.tap(find.text('No'));
-    await tester.pump();
-
-    expect(find.text('Summary'), findsOneWidget);
+      expect(find.text('Summary'), findsOneWidget);
+    });
   });
 }
