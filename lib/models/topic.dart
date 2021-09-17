925247627442a19cbcab0057745a2697c9649309
ken1flan <ken1flan@gmail.com>
2021-09-18 08:50:43 +0900

    Fix Topic#save

--- lib/models/topic.dart
+++ lib/models/topic.dart
@@ -59,7 +59,7 @@ class Topic extends HiveObject {
     updated_at = DateTime.now().toUtc();
     created_at ??= updated_at;
 
-    if (index == null) {
+    if (key == null) {
       return box.add(this);
     } else {
       return super.save();
